5be3e1292e66c600877108c0b022295369619d0a
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2023-10-20 11:45:40 +0200

    working daemon: need to optimize session management

--- srcs/ft_shield/src/main.cpp
+++ srcs/ft_shield/src/main.cpp
@@ -6,53 +6,83 @@
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 16:20:01 by halvarez          #+#    #+#             */
-/*   Updated: 2023/10/19 18:13:01 by halvarez         ###   ########.fr       */
+/*   Updated: 2023/10/20 11:43:11 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+
 #include <iostream>
 #include <string>
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+#define DAEMON_LOG_PATH "/var/log/matt_daemon"
+#define DAEMON_LOCK_FILE "/var/lock/matt_daemon"
 
 void daemonize(void)
 {
-	int pid = 0;
+	int	pid		= 0;
+	int	fd		= 0;
+	char buf[9] = {'\0'};
 
-	// First fork to detach from current processus
+	// First fork to detach from the current processus
 	pid = fork();
 	if (pid == -1)
 		exit(EXIT_FAILURE);
 		//fail
 	else if (pid > 0)
-	{
-		std::cout << "halvarez" << std::endl;
 		exit(EXIT_SUCCESS);
-	}
 	else if (pid == 0)
 	{
+		// Become leader of the processes group
 		setpgid(0, 0);
+		// Create a new session
 		setsid();
 		// Second fork to detach from terminal
 		pid = fork();
 		if (pid == -1)
 			exit(EXIT_FAILURE);
 		else if (pid > 0)
+		{
+			std::cout << "halvarez" << std::endl;
 			exit(EXIT_SUCCESS);
+		}
 		else if (pid == 0)
 		{
-			// Close all file discriptors
-			for (int i = getdtablesize(); i >= 3, i--)
-				close(i);
+			// Close all open file discriptors
+			//for (int i = getdtablesize(); i >= 3; i--)
+			//	close(i);
 			// Redirect stdin, stdout and stderr to /dev/null
 			freopen("/dev/null", "r", stdin);
 			freopen("/dev/null", "w", stdout);
 			freopen("/dev/null", "w", stderr);
+			// Set default permisions
 			umask(0000);
-
+			// Test if log path exists, creates it and moves in otherwise
+			if (chdir(DAEMON_LOG_PATH) == -1)
+			{
+				if (mkdir(DAEMON_LOG_PATH, 0755) == -1)
+					exit(EXIT_FAILURE);
+				if (chdir(DAEMON_LOG_PATH) == -1)
+					exit(EXIT_FAILURE);
+			}
+			// Create a lock file == check if an instance of this daemon is running
+			fd = open(DAEMON_LOCK_FILE, O_RDWR | O_CREAT);
+			if (fd == -1)
+				exit(EXIT_FAILURE);
+			// Lock the file, exit if the file is already locked
+			if (lockf(fd, F_TLOCK, 0) == -1)
+				exit(EXIT_SUCCESS);
+			// Write the daemon pid in the lock file
+			sprintf(buf, "%d\n", getpid());
+			write(fd, buf, strlen(buf));
+			// Infinite loop to do whatever
+			while(1);
 		}
 	}
 	return;
@@ -60,12 +90,6 @@ void daemonize(void)
 
 int main(void)
 {
-	umask(0000);
-	seteuid(0);
-	int res = mkdir("/test", 0755);
-	if ( res == 0 )
-		std::cout << "ok" << std::endl;
-	else
-		std::cout << "fail" << std::endl;
+	daemonize();
 	return 0;
 }
