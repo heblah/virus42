ad16fa6a13b05c1c3db3a8c49cb23bd1c4077f38
pgueugno <pgueugno@student.42.fr>
2023-10-29 18:05:18 +0100

    feat: add makefile and basic working main function

--- srcs/woody_woodpacker/srcs/main.c
+++ srcs/woody_woodpacker/srcs/main.c
@@ -1,7 +1,9 @@
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /*
 * Parse arg error, check if directory, check if binary file or not
@@ -12,5 +14,17 @@
 
 int		main(int argc, char **argv)
 {
+	int	fd;
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_DIRECTORY);
+		if (fd != -1)
+		{
+			printf("Error\nFirst argument is a directory\n");
+			close(fd);
+			exit(0);
+		}
+	}
+	printf("Error\nInvalid number of argument\n");
 	return 0;
 }
