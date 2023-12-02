c53e76315e11a46613d43738d295ae85a114b211
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2023-12-02 15:03:49 +0100

    first famine commit

---
+++ srcs/famine/src/main.c
@@ -0,0 +1,15 @@
+
+/*
+ * 42 project: Famine
+ * Author: halvarez / plgueugnon
+ * Starting date: 01/12/2023
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+	return 0;
+}
