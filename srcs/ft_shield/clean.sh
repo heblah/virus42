de42402c81a763c4262f38bae588aecebef44648
halvarez <hansalvarezherencia@protonmail.com>
2023-12-04 17:11:14 +0100

    clean.sh cleans services

--- srcs/ft_shield/clean.sh
+++ srcs/ft_shield/clean.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sudo systemctl disable kthread
+sudo systemctl stop kthread
 sudo rm /var/lock/.matt_daemon
 sudo rm /etc/systemd/system/kthread.service
 sudo rm /usr/local/bin/kthread
