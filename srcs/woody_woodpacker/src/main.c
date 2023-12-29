d915a17eaf3a0d11b57fc65c501958524490a21a
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2023-12-29 15:50:23 +0100

    bug when reading

---
+++ srcs/woody_woodpacker/src/main.c
@@ -0,0 +1,8 @@
+
+#include "woody.h"
+
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+	mk_cfile(argv[1]);
+	return 0;
+}
---
+++ srcs/woody_woodpacker/src/woody.h
@@ -0,0 +1,7 @@
+
+#ifndef WOODY_H
+#define WOODY_H
+
+int	mk_cfile(const char * file);
+
+#endif
