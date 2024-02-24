c6a7b30990e72b479d447679f0e4a267389481fe
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2024-02-24 12:13:44 +0100

    cleaning files

--- srcs/woody_woodpacker/convert.py
+++
@@ -1,9 +0,0 @@
-
-print("\t\t", end="")
-for i in range(0, 256):
-	if (i + 1) % 12 != 0 and i != 255:
-		print("0x{:02x}".format(i), end=", ")
-	elif (i + 1) % 12 == 0 and i != 255:
-		print("0x{:02x}".format(i), end=",\n\t\t")
-	else:
-		print("0x{:02x}".format(i))
